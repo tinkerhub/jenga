b15d14bc643ed17218cb0b088721bdd641f879ba
Gopikrishnan Sasikumar <gopikrishnans1996@gmail.com>
2020-11-05 20:56:02 +0530

    Created script to set env vars

---
+++ configure.sh
@@ -0,0 +1,6 @@
+python3 setup.py install
+export SECRET_KEY=<KEY>
+export DETA_PROJECT_KEY=<DETA_KEY>
+export DETA_BASE=<BASE>
+export AIRTABLE_API_KEY=<AIRTABLE_KEY>
+export APP_SETTINGS=jenga.config.TestingConfig
