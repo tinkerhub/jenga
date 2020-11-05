6b815104d7fbadff6cc892c0429a94e2bd1ecd2c
Gopikrishnan Sasikumar <gopikrishnans1996@gmail.com>
2020-11-05 19:30:27 +0530

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+requirements = [
+    "airtable-python-wrapper==0.15.0",
+    "deta==0.6",
+    "Flask==1.1.2",
+    "Jinja2==2.11.1",
+    "python-dotenv==0.14.0",
+    "requests==2.23.0",
+    "requests-oauthlib==1.3.0",
+    "Werkzeug==1.0.1",
+    "gunicorn",
+    "twilio"
+]
+
+setup(
+    name="jenga",
+    version="0.0.1",
+    zip_safe=False,
+    packages=find_packages(exclude=['tests']),
+    include_package_data=True,
+    install_requires=requirements)
